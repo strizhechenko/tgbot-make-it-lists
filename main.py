99d3c594967f433ed8f5456fe906c67ad26ebce3
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2023-04-10 09:53:09 +0500

    В основном скрипте не использовался логгер

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 from assist_bot.assist import Assist
 from aiogram import executor
 
-log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
