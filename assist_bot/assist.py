65cf206003ebce0fe1308b354dfdd88093d7114b
Стрижеченко Олег Игоревич <strizhechenko_o@tochka.com>
2023-05-20 12:12:09 +0500

    Уродские staticmethod'ы

--- assist_bot/assist.py
+++ assist_bot/assist.py
@@ -12,14 +12,16 @@ class Assist:
     _known_file = Path('./known.json')
     known = json.loads(_known_file.read_text()) if _known_file.exists() else dict()
 
+    @staticmethod
     @dispatcher.message_handler()
-    async def search(self, message: types.Message):
+    async def search(message: types.Message):
         if message.from_user.username == owner:
             if message.text in Assist.known:
-                return await self.decompose_known(message)
+                return await Assist.decompose_known(message)
             for reply in split(message.text):
                 await Assist.bot.send_message(message.chat.id, reply)
 
-    async def decompose_known(self, message):
-        for reply in self.known[message.text]:
-            await self.bot.send_message(message.chat.id, reply)
+    @staticmethod
+    async def decompose_known(message):
+        for reply in Assist.known[message.text]:
+            await Assist.bot.send_message(message.chat.id, reply)
