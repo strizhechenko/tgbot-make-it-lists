fbddb2499980642629970b2c8a071af410f3addb
Стрижеченко Олег Игоревич <strizhechenko_o@tochka.com>
2023-05-20 12:00:38 +0500

    Добавлена поддержка декомпозиции часто используемых параметров по одному слову.

--- assist_bot/assist.py
+++ assist_bot/assist.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from aiogram import Bot, Dispatcher, types
 
 from assist_bot.config import token, owner
@@ -7,10 +9,17 @@ from assist_bot.splitter import split
 class Assist:
     bot = Bot(token=token)
     dispatcher = Dispatcher(bot=bot)
+    _known_file = Path('./known.json')
+    known = json.loads(_known_file.read_text()) if _known_file.exists() else dict()
 
-    @staticmethod
     @dispatcher.message_handler()
-    async def search(message: types.Message):
+    async def search(self, message: types.Message):
         if message.from_user.username == owner:
+            if message.text in Assist.known:
+                return await self.decompose_known(message)
             for reply in split(message.text):
                 await Assist.bot.send_message(message.chat.id, reply)
+
+    async def decompose_known(self, message):
+        for reply in self.known[message.text]:
+            await self.bot.send_message(message.chat.id, reply)
