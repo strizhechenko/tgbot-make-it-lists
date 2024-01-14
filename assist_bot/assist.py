75017425099d9fc5d50b36b6a808a7d6df9d3845
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2024-01-14 19:47:42 +0500

    Добавил поддержку других временных запросов задач на интервал времени: - сегодня - завтра - послезавтра - эта неделя - следующая неделя - вчера.

--- assist_bot/assist.py
+++ assist_bot/assist.py
@@ -1,6 +1,6 @@
 import json
 import locale
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from aiogram import Bot, Dispatcher, types
@@ -22,8 +22,8 @@ class Assist:
         if message.from_user.username == config.OWNER:
             if message.text in Assist.known:
                 return await Assist.decompose_known(message)
-            if message.text == 'сегодня':
-                return await Assist.today_agenda(message)
+            if message.text in ('сегодня', 'завтра', 'послезавтра', 'вчера', 'эта неделя', 'следующая неделя'):
+                return await Assist.agenda(message)
             for reply in split(message.text):
                 await Assist.bot.send_message(message.chat.id, reply)
 
@@ -33,14 +33,12 @@ class Assist:
             await Assist.bot.send_message(message.chat.id, reply)
 
     @staticmethod
-    async def today_agenda(message):
+    async def agenda(message):
         if not Assist._agenda_file.exists():
             await Assist.bot.send_message(message.chat.id, "Никаких планов вообще")
             return
 
-        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
-        day_of_week = datetime.now().strftime('%A')
-        query = ['# Эта неделя', '## ' + day_of_week]
+        query = Assist.markdown_query_from(message.text.lower())
         text = Assist._agenda_file.read_text(encoding='utf-8')
         result = markdown_checklist_lookup(text, query)
         results = split(result)
@@ -50,3 +48,32 @@ class Assist:
 
         for reply in results:
             await Assist.bot.send_message(message.chat.id, reply)
+
+    @staticmethod
+    def markdown_query_from(text: str) -> list[str]:
+        """
+        >>> import freezegun
+        >>> with freezegun.freeze_time('2024.01.14'): Assist.markdown_query_from('сегодня')
+        ['# Эта неделя', '## Воскресенье']
+        >>> with freezegun.freeze_time('2024.01.14'): Assist.markdown_query_from('завтра')
+        ['# Следующая неделя', '## Понедельник']
+        >>> with freezegun.freeze_time('2024.01.14'): Assist.markdown_query_from('послезавтра')
+        ['# Следующая неделя', '## Вторник']
+        >>> with freezegun.freeze_time('2024.01.15'): Assist.markdown_query_from('послезавтра')
+        ['# Эта неделя', '## Среда']
+        >>> with freezegun.freeze_time('2024.01.14'): Assist.markdown_query_from('вчера')
+        ['# Эта неделя', '## Суббота']
+        """
+        if text in ('эта неделя', 'следующая неделя'):
+            return ['# ' + text.capitalize()]
+        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+        today = datetime.now()
+        day = today + timedelta(days={
+            'сегодня': 0,
+            'завтра': 1,
+            'послезавтра': 2,
+            'вчера': -1,
+        }.get(text))
+        day_of_week = day.strftime('%A')
+        week = '# Эта неделя' if today.isoweekday() <= day.isoweekday() or text == 'вчера' else '# Следующая неделя'
+        return [week, '## ' + day_of_week]
